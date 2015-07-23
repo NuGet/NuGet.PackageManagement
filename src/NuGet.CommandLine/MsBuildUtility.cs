d12829fc5cc01533685b7942ef133636a6c7dbff
Pranav K <prkrishn@hotmail.com>
2015-07-23 12:02:12 -0700

    Use MsBuild.exe instead of relying on MsBuild APIs to determine referenced projects.
    Fixes https://github.com/NuGet/Home/issues/956

--- src/NuGet.CommandLine/MsBuildUtility.cs
+++ src/NuGet.CommandLine/MsBuildUtility.cs
@@ -3,9 +3,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
-using Microsoft.Build.Execution;
 using NuGet.Common;
 
 namespace NuGet.CommandLine
@@ -26,41 +27,74 @@ namespace NuGet.CommandLine
             return _msbuildExtensions.Contains(Path.GetExtension(projectFullPath));
         }
 
-        public static IEnumerable<string> GetProjectReferences(string projectFullPath)
+        public static IEnumerable<string> GetProjectReferences(string msbuildPath, string projectFullPath)
         {
-            var projectDirectory = Path.GetDirectoryName(projectFullPath);
-            var project = new ProjectInstance(projectFullPath);
-
-            var manager = new BuildManager();
-            var result = manager.Build(
-                new BuildParameters(),
-                new BuildRequestData(project, new[] { GetProjectReferenceBuildTarget }));
-
-            TargetResult targetResult;
-            if (result.ResultsByTarget.TryGetValue(GetProjectReferenceBuildTarget, out targetResult))
+            if (string.IsNullOrEmpty(msbuildPath))
             {
-                if (targetResult.ResultCode == TargetResultCode.Success &&
-                    targetResult.Items.Length > 0)
-                {
-                    var taskItem = targetResult.Items[0];
-                    var metadataValue = taskItem.GetMetadata(ProjectReferencesKey);
+                msbuildPath = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86),
+                    "msbuild",
+                    "14.0",
+                    "bin",
+                    "msbuild.exe");
+            }
 
-                    var projectRelativePaths = metadataValue.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-                    foreach (var relativePath in projectRelativePaths)
+            if (!File.Exists(msbuildPath))
+            {
+                throw new CommandLineException(
+                    string.Format(
+                        CultureInfo.CurrentCulture,
+                        NuGetResources.MsBuildDoesNotExistAtPath,
+                        msbuildPath));
+            }
+
+            var targetPath = Path.GetTempFileName();
+            using (var input = typeof(MsBuildUtility).Assembly.GetManifestResourceStream("NuGet.CommandLine.GetProjectsReferencingProjectJsonFiles.target"))
+            {
+                using (var output = File.OpenWrite(targetPath))
+                {
+                    input.CopyTo(output);
+                }
+            }
+
+            var resultsPath = Path.GetTempFileName();
+            var arguments = $"/p:BuildProjectReferences=false /v:M /p:CustomAfterMicrosoftCommonTargets={targetPath} " +
+                "/t:NuGet_GetProjectsReferencingProjectJson " +
+                $"/nologo /nr:false /p:ResultsFile={resultsPath} {projectFullPath}";
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                UseShellExecute = false,
+                FileName = msbuildPath,
+                Arguments = arguments,
+                RedirectStandardError = true
+            };
+            try
+            {
+                using (var process = Process.Start(processStartInfo))
+                {
+                    process.WaitForExit(60 * 1000);
+
+                    if (process.ExitCode != 0)
                     {
-                        var resolvedPath = Path.GetFullPath(Path.Combine(projectDirectory, relativePath));
-                        yield return resolvedPath;
+                        throw new CommandLineException(process.StandardError.ReadToEnd());
                     }
                 }
-                else if (targetResult.Exception != null)
+
+                if (File.Exists(resultsPath))
                 {
-                    throw targetResult.Exception;
+                    return File.ReadAllLines(resultsPath)
+                        .Where(file => !string.Equals(projectFullPath, file, StringComparison.OrdinalIgnoreCase))
+                        .Distinct(StringComparer.OrdinalIgnoreCase);
                 }
             }
-            else if (result.Exception != null)
+            finally
             {
-                throw result.Exception;
+                File.Delete(targetPath);
+                File.Delete(resultsPath);
             }
+
+            return Enumerable.Empty<string>();
         }
 
         public static IEnumerable<string> GetAllProjectFileNames(string solutionFile)
